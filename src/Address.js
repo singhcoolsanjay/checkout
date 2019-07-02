ef211f630323f53a087fbb4b0a18bf64d40e7bb9
singhcoolsanjay <singhcoolsanjay@gmail.com>
2019-07-03 03:47:58 +0530

    Create Address.js

---
+++ src/Address.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function SimpleTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Exisitng Address" />
+          <Tab label="New Address" />
+          
+        </Tabs>
+      </AppBar>
+      {value === 0 && <TabContainer>Value from grid</TabContainer>}
+      {value === 1 && <TabContainer>Value from new from</TabContainer>}
+      
+    </div>
+  );
+}
